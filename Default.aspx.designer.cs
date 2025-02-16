f3ee6c72d0654447ba3f6902e7b756bf8ef6d593
Aviral Malik <82748379+floofy244@users.noreply.github.com>
2025-02-16 20:15:59 +0530

    Added Update functionality

--- Default.aspx.designer.cs
+++ Default.aspx.designer.cs
@@ -112,14 +112,5 @@ namespace Aviral_ASP
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnSubmit;
-
-        /// <summary>
-        /// viewData control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button viewData;
     }
 }
--- userData.aspx.cs
+++ userData.aspx.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Data.SqlClient;
 using System.Data;
-using System.Linq;
-using System.Net;
-using System.Web;
+using System.Data.SqlClient;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -14,6 +10,7 @@ namespace Aviral_ASP
     public partial class userData : Page
     {
         private string connectionString = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -44,5 +41,81 @@ namespace Aviral_ASP
             Response.Redirect("Default.aspx");
         }
 
+       
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            LoadData();
+        }
+
+        
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            LoadData();
+        }
+
+        
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+
+            string firstName = ((TextBox)row.Cells[2].Controls[0]).Text;
+            string middleName = ((TextBox)row.Cells[3].Controls[0]).Text;
+            string lastName = ((TextBox)row.Cells[4].Controls[0]).Text;
+            string PAddress = ((TextBox)row.Cells[5].Controls[0]).Text;
+            string CAddress = ((TextBox)row.Cells[6].Controls[0]).Text;
+            string phone = ((TextBox)row.Cells[7].Controls[0]).Text;
+            DateTime dob = DateTime.Parse(((TextBox)row.Cells[8].Controls[0]).Text);
+            string age = ((TextBox)row.Cells[9].Controls[0]).Text;
+            string status = ((TextBox)row.Cells[10].Controls[0]).Text;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Tbl_Aman_Test SET F_Name=@FirstName,M_Name=@middleName, L_Name=@LastName, P_Address=@PAddress, C_Address=@CAddress, Mobile=@PhoneNumber, DOB=@DOB, Age=@Age, Status=@StatusFlag WHERE ID=@ID";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ID", id);
+                    cmd.Parameters.AddWithValue("@FirstName", firstName);
+                    cmd.Parameters.AddWithValue("@middleName", middleName);
+                    cmd.Parameters.AddWithValue("@LastName", lastName);
+                    cmd.Parameters.AddWithValue("@PAddress", PAddress);
+                    cmd.Parameters.AddWithValue("@CAddress", CAddress);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", phone);
+                    cmd.Parameters.AddWithValue("@DOB", dob);
+                    cmd.Parameters.AddWithValue("@Age", age);
+                    cmd.Parameters.AddWithValue("@StatusFlag", status);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            GridView1.EditIndex = -1;
+            LoadData();
+        }
+
+      
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            //int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+
+
+            //using (SqlConnection conn = new SqlConnection(connectionString))
+            //{
+                //string query = "DELETE FROM Tbl_Aman_Test WHERE ID=@ID";
+
+                //using (SqlCommand cmd = new SqlCommand(query, conn))
+                //{
+                    //cmd.Parameters.AddWithValue("@ID", id);
+                   // conn.Open();
+                    //cmd.ExecuteNonQuery();
+                //}
+            //}
+
+            LoadData();
+        }
     }
 }
